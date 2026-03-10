f2918126610cf906dea07cbc1bd1abce437966be
Feddas <1483568+Feddas@users.noreply.github.com>
2026-03-09 21:58:16 -0700

    test: SpriteBatch parameters

--- MonoSnake/Game1.cs
+++ MonoSnake/Game1.cs
@@ -7,6 +7,8 @@ namespace MonoSnake;
 
 public class Game1 : Core
 {
+    private Vector2 _screenCenter;
+    private Vector2 _logoCenter;
     private Texture2D _logo;
 
     private GraphicsDeviceManager _graphics;
@@ -24,7 +26,10 @@ public class Game1 : Core
 
     protected override void LoadContent()  // script order #3-ish, called via base.Initialize();
     {
+        _screenCenter = new Vector2(Window.ClientBounds.Width * 0.5f, Window.ClientBounds.Height * 0.5f);
+
         _logo = Content.Load<Texture2D>("images/mgLogo");
+        _logoCenter = new Vector2(_logo.Width * 0.5f, _logo.Height * 0.5f);
         //base.LoadContent();
     }
 
@@ -42,11 +47,28 @@ public class Game1 : Core
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // Begin the sprite batch to prepare for rendering.
-        SpriteBatch.Begin();
+        //// The bounds of the icon within the texture.
+        //Rectangle iconSourceRect = new Rectangle(0, 0, 128, 128);
 
-        // Draw the logo texture
-        SpriteBatch.Draw(_logo, Vector2.Zero, Color.White);
+        //// The bounds of the word mark within the texture.
+        //Rectangle wordmarkSourceRect = new Rectangle(150, 34, 458, 58);
+
+        // Begin the sprite batch to prepare for rendering.
+        SpriteBatch.Begin(SpriteSortMode.FrontToBack);
+
+        // Draw the texture. https://docs.monogame.net/api/Microsoft.Xna.Framework.Graphics.SpriteBatch.html#Microsoft_Xna_Framework_Graphics_SpriteBatch_Draw_Microsoft_Xna_Framework_Graphics_Texture2D_Microsoft_Xna_Framework_Vector2_Microsoft_Xna_Framework_Color_
+        SpriteBatch.Draw(
+            _logo,              // texture
+            _screenCenter,      // position
+            null,               // sourceRectangle
+            Color.White,        // color
+            MathHelper.ToRadians(gameTime.TotalGameTime.Milliseconds * .1f),               // rotation
+            _logoCenter,        // origin / pivot for rotation and scale
+            1.0f,               // scale
+            SpriteEffects.None, // effects
+            0.0f                // layerDepth
+        );
+
 
         // Always end the sprite batch when finished.
         SpriteBatch.End();
